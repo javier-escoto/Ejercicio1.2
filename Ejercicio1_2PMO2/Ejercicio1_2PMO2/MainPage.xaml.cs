3554ccce24cb1905a3f4aec731fa367db881b074
javie <javie@DESKTOP-KAOB9VG>
2023-02-27 22:08:42 -0600

    Add project files.

---
+++ Ejercicio1_2PMO2/Ejercicio1_2PMO2/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Ejercicio1_2PMO2.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SQLite;
+
+namespace Ejercicio1_2PMO2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnenviar_Clicked(object sender, EventArgs e)
+        {
+            if (txtnombre.Text == null || txtapellidos.Text == null || txtedad.Text == null || txtcorreo.Text == null)
+            {
+                await DisplayAlert("Error", "Campos Vacios", "OK");
+            }
+            else
+            {
+                var usua = new Models.Usuario { id = 1, nombre = txtnombre.Text, apellidos = txtapellidos.Text, edad = txtedad.Text, correo = txtcorreo.Text };
+
+                var pagetwo = new PageTwo();
+                pagetwo.BindingContext = usua;
+
+                await Navigation.PushAsync(pagetwo);
+            }
+            
+            
+            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
---
+++ Ejercicio1_2PMO2/Ejercicio1_2PMO2/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Ejercicio1_2PMO2.Models
+{
+    public class Usuario
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { set; get; }
+
+        [MaxLength(100)]
+        public String nombre { set; get; }
+
+        [MaxLength(100)]
+        public String apellidos { set; get; }
+
+        public String edad { set; get; }
+
+        public String correo { set; get; }
+    }
+}
